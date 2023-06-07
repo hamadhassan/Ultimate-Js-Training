e2e3320b6720b4eb135e4a45f93ce213b8d4e6dd
Muhammad Hammad Hassan <muhammadhammad.hassan@studentambassadors.com>
2023-06-07 16:35:34 +0500

    Chapter-01 Practice Set-01 Solution

---
+++ Chapter 1/chapter-01.js
@@ -0,0 +1,21 @@
+var a=10;
+{
+  let a=20;
+  console.log(a)
+}
+console.log(a)
+
+
+var a=undefined 
+//It work fine
+let b=undefined
+b=10
+console.log(b)
+console.log("this is new line")
+
+const item={
+  "ali":true,
+  "ali2":20,
+  20:10  
+}
+console.log(item[20])
---
+++ Chapter 1/practiceSet-01.js
@@ -0,0 +1,31 @@
+////Practice problemm -01
+////Chapter-01 Problem-01
+let a="abc"
+console.log(a+1)
+////Chapter-01 Problem-02
+console.log(typeof(a))
+console.log(typeof(a+1))
+////Chapter-01 Problem-03
+const item={
+  a:1,
+  b:2,
+  "c":3
+}
+console.log("We not able to hold the number later")
+////Chapter-01 Problem-04
+item['d']=4
+item['a']=0
+console.log(item)
+////Chapter-01 Problem-05
+const meaning={
+  fioritura:"the ornamentation of a melody, often extemporized by the performer, as in Italian opera during the 18th century.",
+  mauka:"toward the mountains",
+  telekinesis:"psychokinesis."
+}
+console.log(meaning["fioritura"])
+
+
+
+
+
+
