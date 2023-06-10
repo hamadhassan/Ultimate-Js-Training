3334897a788850c536800d401491f061219a0ce3
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2023-06-10 09:21:45 +0500

    practice set-04

---
+++ Chapter 4/chapter-04.js
@@ -0,0 +1,6 @@
+let name = "Hamad"
+console.log(name.length)
+console.log(name[0])
+
+let friend = 'Muneeb'
+console.log(friend.length)
---
+++ Chapter 4/probelmSet-04.js
@@ -0,0 +1,16 @@
+////Practice problem -04
+////Chapter-04 Problem-01
+console.log("har\"".length)
+////Chapter-04 Problem-02
+const str="This is "
+const newStr=str.includes('This')
+console.log(newStr)
+console.log(str.startsWith('This'))
+console.log(str.endsWith('is '))
+////Chapter-04 Problem-03
+console.log(str.toLowerCase())
+////Chapter-04 Problem-04
+let str1="Please give me Rs 100 "
+console.log(str1.slice(18))
+////Chapter-04 Problem-05
+console.log(str1[3]) //String is immutable
